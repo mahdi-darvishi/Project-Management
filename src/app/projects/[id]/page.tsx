c2ca59055b676b8c9a293e5b8cfce2234805931e
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-10-09 14:55:08 +0330

    refactor: simplify ProjectPage component by removing unused state and integrating ProjectClient

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,28 +1,13 @@
-"use client";
-
-import { useState } from "react";
-
-import Projectheader from "@/app/projects/Projectheader";
+import ProjectClient from "./ProjectClient";
 
 type Props = {
   params: { id: string };
 };
-const Project = ({ params }: Props) => {
-  const { id } = params;
 
-  const [activeTab, setActiveTab] = useState("Board");
-  const [isModelNewTaskOpen, setIsModelNewTaskOpen] = useState(false);
+const ProjectPage = async ({ params }: Props) => {
+  const { id } = await params;
 
-  return (
-    <div>
-      {/* MODAL NEW TASK */}
-
-      <Projectheader activeTab={activeTab} setActiveTab={setActiveTab} />
-      {/* {activeTab ==="Board" &&(
-        <Board/>
-      )} */}
-    </div>
-  );
+  return <ProjectClient id={id} />;
 };
 
-export default Project;
+export default ProjectPage;
