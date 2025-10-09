b2c21a3a57a291e0f62b1ee7cb08a4f6c8cf337f
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-10-09 14:56:29 +0330

    feat: add BoardView component with drag-and-drop task management and status columns

---
+++ src/app/projects/BoardVeiw/index.tsx
@@ -0,0 +1,202 @@
+import { useGetTasksQuery, useUpdateTaskStatusMutation } from "@/state/api";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+import { Task as TaskType } from "@/state/api";
+import { EllipsisVertical, Plus } from "lucide-react";
+import { format } from "date-fns";
+import Image from "next/image";
+
+type boardProps = {
+  id: string;
+  setIsModelNewTaskOpen: (isOpen: boolean) => void;
+};
+
+const taskStatus = ["To Do", "Work In Progress", "Under Review", "Completed"];
+
+const BoardView = ({ id, setIsModelNewTaskOpen }: boardProps) => {
+  const {
+    data: tasks,
+    isLoading,
+    error,
+  } = useGetTasksQuery({ projectId: Number(id) });
+
+  const [updatetaskStatus] = useUpdateTaskStatusMutation();
+
+  const moveTask = (taskId: number, toStatus: string) => {
+    updatetaskStatus({ taskId, status: toStatus });
+  };
+
+  if (isLoading) return <div>loading ...</div>;
+  if (error) return <div>An error occurred while fetching task</div>;
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 xl:grid-cols-4">
+        {taskStatus.map((status) => (
+          <TaskColumn
+            key={status}
+            status={status}
+            tasks={tasks || []}
+            moveTask={moveTask}
+            setIsModelNewTaskOpen={setIsModelNewTaskOpen}
+          />
+        ))}
+      </div>
+    </DndProvider>
+  );
+};
+
+type taslColumnProps = {
+  status: string;
+  tasks: TaskType[];
+  moveTask: (taskId: number, toStatus: string) => void;
+  setIsModelNewTaskOpen: (isOpne: boolean) => void;
+};
+
+const TaskColumn = ({
+  status,
+  tasks,
+  moveTask,
+  setIsModelNewTaskOpen,
+}: taslColumnProps) => {
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "task",
+    drop: (item: { id: number }) => moveTask(item.id, status),
+    // eslint-disable-next-line
+    collect: (monitor: any) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const tasksCount = tasks.filter((task) => task.status === status).length;
+  // eslint-disable-next-line
+  const statusColor: any = {
+    "To Do": "#2563EB",
+    "Work In Progress": "#059669",
+    "Under Review": "#D97706",
+    Completed: "#000000",
+  };
+
+  return (
+    <div
+      ref={(instance) => {
+        drop(instance);
+      }}
+      className={`rounded-lg py-2 xl:px-2 ${isOver ? "bg-blue-100 dark:bg-neutral-950" : ""}`}
+    >
+      <div className="mb-3 flex w-full">
+        <div
+          className={`w-2 !bg-[${statusColor[status]}] rounded-s-lg`}
+          style={{ backgroundColor: statusColor[status] }}
+        />
+
+        <div className="dark:bg-dark-secondary flex w-full items-center justify-between rounded-e-lg bg-white px-5 py-4">
+          <h3 className="flex items-center text-lg font-semibold dark:text-white">
+            {status}{" "}
+            <span
+              className="dark:bg-dark-tertiary ml-2 inline-block rounded-full bg-gray-200 p-1 text-center text-sm leading-none"
+              style={{ width: "1.5rem", height: "1.5rem" }}
+            >
+              {tasksCount}
+            </span>
+          </h3>
+
+          <div className="flex items-center gap-1">
+            <button className="flex h-6 w-5 cursor-pointer items-center justify-center dark:text-neutral-500">
+              <EllipsisVertical size={26} />
+            </button>
+            <button
+              className="roudned dark:bg-dark-tertiary flex h-6 w-6 cursor-pointer items-center justify-center bg-gray-200 dark:text-white"
+              onClick={() => setIsModelNewTaskOpen(true)}
+            >
+              <Plus size={16} />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {tasks
+        .filter((task) => task.status === status)
+        .map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+    </div>
+  );
+};
+
+type TaskProps = {
+  task: TaskType;
+};
+
+const Task = ({ task }: TaskProps) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "task",
+    item: { id: task.id },
+    // eslint-disable-next-line
+    collect: (monitor: any) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  const taskTagsSplit = task.tags ? task.tags.split(",") : [];
+
+  const formattedStartDate = task.startDate
+    ? format(new Date(task.startDate), "p")
+    : "";
+
+  const formattedDueDate = task.dueDate
+    ? format(new Date(task.dueDate), "p")
+    : "";
+
+  const numberOfComments = (task.comments && task.comments.length) || 0;
+
+  const PriorityTag = ({ priority }: { priority: TaskType["priority"] }) => (
+    <div
+      className={`rounded-full px-2 py-1 text-xs font-semibold ${priority === "Urgent" ? "bg-red-200 text-red-700" : priority === "High" ? "bg-yellow-200 text-yellow-700" : priority === "Medium" ? "bg-green-200 text-green-700" : priority === "Low" ? "bg-blue-200 text-blue-700" : "bg-gray-200 text-gray-700"}`}
+    >
+      {priority}
+    </div>
+  );
+
+  return (
+    <div
+      ref={(instance) => {
+        drag(instance);
+      }}
+      className={`dark:bg-dark-secondary mb-4 rounded-md bg-white shadow ${
+        isDragging ? "opacity-50" : "opacity-100"
+      }`}
+    >
+      {task.attachments && task.attachments.length > 0 && (
+        <Image
+          src={`/${task.attachments[0].fileurl}`}
+          alt={task.attachments[0].fileName}
+          width={400}
+          height={200}
+          className="h-auto w-full rounded-t-md"
+        />
+      )}
+
+      <div className="p-4 md:p-6">
+        <div className="flex items-start justify-between">
+          <div className="flex flex-1 flex-wrap items-center gap-2">
+            {task.priority && <PriorityTag priority={task.priority} />}
+            <div className="flex gap-2">
+              {taskTagsSplit.map((tag) => (
+                <div
+                  key={tag}
+                  className="rounded-full bg-blue-100 px-2 py-1 text-xs"
+                >
+                  {tag}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BoardView;
