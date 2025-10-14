4825aea67519703bdf5532a41c4c179d95108e7e
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-10-14 14:05:29 +0330

    fix: enhance Modal component styling for dark mode and cursor pointer

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -14,7 +14,7 @@ const Modal = ({ children, isOpen, onClose, name }: Props) => {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 z-50 flex h-full w-full items-center justify-center overflow-y-auto bg-gray-600/50 p-4">
+    <div className="dark:bg-dark-secondary/70 fixed inset-0 z-50 flex h-full w-full items-center justify-center overflow-y-auto bg-gray-600/50 p-4">
       <div className="dark:bg-dark-secondary w-full max-w-2xl rounded-lg bg-white p-4 shadow-lg">
         <Header
           name={name}
@@ -23,7 +23,7 @@ const Modal = ({ children, isOpen, onClose, name }: Props) => {
               className="bg-blue-primary flex h-7 w-7 items-center justify-center rounded-full text-white hover:bg-blue-600"
               onClick={onClose}
             >
-              <X size={18} />
+              <X size={18} className="cursor-pointer" />
             </button>
           }
           isSmallText
