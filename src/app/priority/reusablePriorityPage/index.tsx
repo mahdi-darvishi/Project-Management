b4b8fa6e446a335fee25b1089016f4b1f98f3355
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-10-16 11:19:01 +0330

    feat: create src/app/priority/reusablePriorityPage/index.tsx

---
+++ src/app/priority/reusablePriorityPage/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Priority } from "@/state/api";
+import React from "react";
+
+type Props = {
+  priority: Priority;
+};
+
+const index = ({ priority }: Props) => {
+  return <div>index</div>;
+};
+
+export default index;
