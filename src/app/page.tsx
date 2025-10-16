29625bb6a0fc6625623db3c507ea147b5c08ebbc
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-10-16 11:18:46 +0330

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import HomePage from "./home/page";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-black dark:text-white">
-      hi
-    </main>
-  );
+  return <HomePage />;
 }
