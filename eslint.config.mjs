16932c63d04bfa6395e97261c23acd1f2ff6359c
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-10-16 19:08:45 +0330

    update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,24 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    // اضافه کردن قوانین مورد نیاز در این قسمت
+    rules: {
+      // 1. غیرفعال کردن خطای استفاده از 'any' (Error)
+      "@typescript-eslint/no-explicit-any": "off",
+
+      // 2. تنظیم اخطار برای متغیرهای استفاده نشده، به طوری که متغیرهای شروع شده با '_' نادیده گرفته شوند (Warning)
+      // این روش بهتر از "off" کردن کامل است.
+      "@typescript-eslint/no-unused-vars": [
+        "warn", // می‌توانید به جای "warn" از "off" استفاده کنید اگر می‌خواهید کاملاً غیرفعال شود
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
